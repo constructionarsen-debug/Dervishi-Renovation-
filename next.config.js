df9ad7c7bc2783b08cfa47f1dda4058bb33aef60
Neli <info@duadev.al>
2026-02-28 14:40:47 +0100

    .ufs.sh

--- next.config.js
+++ next.config.js
@@ -4,12 +4,17 @@ const nextConfig = {
     // Allow common external image hosts used in this project
     // (UploadThing, Unsplash, Google avatars, etc.)
     remotePatterns: [
-      { protocol: 'https', hostname: 'utfs.io' },
-      { protocol: 'https', hostname: 'uploadthing.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }
-    ]
-  }
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      {
+        protocol: "https",
+        hostname: "**.ufs.sh",
+        pathname: "/f/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
